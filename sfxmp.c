7f44dc57a75396d481849ecc35ade5d844d9b153
Clément Bœsch <clement@stupeflix.com>
2015-09-04 11:36:12 +0200

    sfxmp: remove now useless condition around debug printing

--- sfxmp.c
+++ sfxmp.c
@@ -563,8 +563,7 @@ static int seek_to(struct sfxmp_ctx *s, double t)
     const double vt = get_media_time(s, t);
     const int64_t ts = vt * AV_TIME_BASE;
 
-    if (ENABLE_DBG)
-        DBG("Seek in media (%s) at %f (t=%f)\n", s->filename, vt, t);
+    DBG("Seek in media (%s) at %f (t=%f)\n", s->filename, vt, t);
     return avformat_seek_file(s->fmt_ctx, -1, INT64_MIN, ts, ts, 0);
 }
 
