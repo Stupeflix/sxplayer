3d3916b6b0609faa2a68b830a47437bc5f13132b
Clément Bœsch <clement@stupeflix.com>
2016-01-22 12:13:20 +0100

    decoder_vt: remove uneeded Picture ifdefery

--- decoder_vt.c
+++ decoder_vt.c
@@ -19,9 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#define Picture QuickdrawPicture
 #include <VideoToolbox/VideoToolbox.h>
-#undef Picture
 
 #include "decoding.h"
 #include "decoders.h"
