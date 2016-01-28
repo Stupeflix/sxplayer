c6420e86e2ab75abd29965209d44fc6aa089a5a3
Clément Bœsch <clement@stupeflix.com>
2016-01-28 11:41:12 +0100

    log: reindent after previous commit

--- log.c
+++ log.c
@@ -32,7 +32,6 @@ void do_log(void *log_ctx, int log_level, const char *fn, const char *fmt, ...)
         ctx->callback(ctx->user_arg, log_level, fmt, arg_list);
         va_end(arg_list);
     } else {
-        // TODO reindent
         char logline[512];
         static const int av_log_levels[] = {
             [SXPLAYER_LOG_VERBOSE] = AV_LOG_VERBOSE,
@@ -42,17 +41,16 @@ void do_log(void *log_ctx, int log_level, const char *fn, const char *fmt, ...)
             [SXPLAYER_LOG_ERROR]   = AV_LOG_ERROR,
         };
         const int av_log_level = av_log_levels[log_level];
+        int64_t t = av_gettime();
 
-    int64_t t = av_gettime();
+        if (!ctx->last_time)
+            ctx->last_time = t;
 
-    if (!ctx->last_time)
+        va_start(arg_list, fmt);
+        vsnprintf(logline, sizeof(logline), fmt, arg_list);
+        va_end(arg_list);
+
+        av_log(ctx->avlog, av_log_level, "[%f] %s: %s\n", (t - ctx->last_time) / 1000000., fn, logline);
         ctx->last_time = t;
-
-    va_start(arg_list, fmt);
-    vsnprintf(logline, sizeof(logline), fmt, arg_list);
-    va_end(arg_list);
-
-    av_log(ctx->avlog, av_log_level, "[%f] %s: %s\n", (t - ctx->last_time) / 1000000., fn, logline);
-    ctx->last_time = t;
     }
 }
