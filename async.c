de3aebce167417f5c207dde4cbc763bbe538ea50
Clément Bœsch <clement@stupeflix.com>
2016-07-26 12:49:02 +0200

    async: add some logging related to seek

--- async.c
+++ async.c
@@ -479,8 +479,10 @@ static int op_start(struct async_context *actx)
     }
 
     if (actx->request_seek != AV_NOPTS_VALUE) {
+        TRACE(actx, "request seek is set to %s", PTS2TIMESTR(actx->request_seek));
         seek_to = actx->request_seek;
     } else if (actx->s->skip64) {
+        TRACE(actx, "skip is set to %s", actx->s->skip64);
         seek_to = actx->s->skip64;
     }
 
