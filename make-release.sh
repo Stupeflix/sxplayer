c735dbded9feb5e8f9463d97148d0de58f61f026
Clément Bœsch <clement@stupeflix.com>
2016-09-28 12:04:23 +0200

    release: add clumsy make-release.sh script

---
+++ make-release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+extract(){
+	echo $1 | cut -d. -f $2
+}
+VERSION=$1
+MAJOR=$(extract $VERSION 1)
+MINOR=$(extract $VERSION 2)
+MICRO=$(extract $VERSION 3)
+
+sed "s/^Version: .*/Version: $VERSION/" -i libsxplayer.pc.tpl
+sed -e "s/^#define SXPLAYER_VERSION_MAJOR.*/#define SXPLAYER_VERSION_MAJOR $MAJOR/" \
+    -e "s/^#define SXPLAYER_VERSION_MINOR.*/#define SXPLAYER_VERSION_MINOR $MINOR/" \
+    -e "s/^#define SXPLAYER_VERSION_MICRO.*/#define SXPLAYER_VERSION_MICRO $MICRO/" \
+    -i sxplayer.h
+
+git commit -a -m "Release $VERSION"
+git tag "v$VERSION"
