d878c00f8ee786dc20dac1d55c93705e6f6db477
Clément Bœsch <cboesch@gopro.com>
2018-12-06 11:09:48 +0100

    Release 9.3.0

--- sxplayer.h
+++ sxplayer.h
@@ -25,7 +25,7 @@
 #include <stdarg.h>
 
 #define SXPLAYER_VERSION_MAJOR 9
-#define SXPLAYER_VERSION_MINOR 2
+#define SXPLAYER_VERSION_MINOR 3
 #define SXPLAYER_VERSION_MICRO 0
 
 #define SXPLAYER_GET_VERSION(major, minor, micro) ((major)<<16 | (minor)<<8 | (micro))
