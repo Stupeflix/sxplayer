93db28ec38fd1300272f99e1800bc8528d57e5f3
Matthieu Bouron <matthieu.bouron@gmail.com>
2019-10-22 10:18:11 +0200

    Release 9.4.0

--- libsxplayer.pc.tpl
+++ libsxplayer.pc.tpl
@@ -5,7 +5,7 @@ libdir=${exec_prefix}/lib
 
 Name: sxplayer
 Description: Stupeflix Player library
-Version: 9.3.2
+Version: 9.4.0
 Cflags: -I${includedir}
 Libs: -L${libdir} -lsxplayer DEP_LIBS
 Libs.private: DEP_PRIVATE_LIBS
--- sxplayer.h
+++ sxplayer.h
@@ -25,8 +25,8 @@
 #include <stdarg.h>
 
 #define SXPLAYER_VERSION_MAJOR 9
-#define SXPLAYER_VERSION_MINOR 3
-#define SXPLAYER_VERSION_MICRO 2
+#define SXPLAYER_VERSION_MINOR 4
+#define SXPLAYER_VERSION_MICRO 0
 
 #define SXPLAYER_GET_VERSION(major, minor, micro) ((major)<<16 | (minor)<<8 | (micro))
 
